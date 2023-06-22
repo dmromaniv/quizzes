e28c18115a8eb11e0e6ba2da5819fa80245f4353
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-06-22 03:26:27 +0300

    updated config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  server: {
+    host: true,
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
